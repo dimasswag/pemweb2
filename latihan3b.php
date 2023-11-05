d2cc79ee5b0aeaf68c3b3f32ba3ce02a147b449d
dimasswag <148695188+dimasswag@users.noreply.github.com>
2023-11-06 00:42:55 +0700

    Add files via upload

---
+++ latihan3b.php
@@ -0,0 +1,15 @@
+<?php
+$jawabanlsset = "Isset adalah fungsi yang digunakan untuk memeriksa apakah sesuatu variabel telah di atur dan memiliki nilai. <br><br>";
+$jawabanEmpty = "empty adalah fungsi yang digunakan untuk memeriksa apakah sesuatu variabel kosong.";
+
+
+   if(isset($jawabanIsset)){
+       echo "$jawabanIsset";
+   }
+
+   if(isset($jawabanEmpty))
+
+
+
+
+?>
